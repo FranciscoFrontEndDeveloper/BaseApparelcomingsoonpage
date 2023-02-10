b057a6906c9be730425d8a22f0a462dab6ed34d8
FRANCISCO <pachonetworking@gmail.com>
2023-02-09 20:08:11 -0500

    footer component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { SectionOneComponent } from './components/section-one/section-one.component';
+import { FootercomponentComponent } from './components/footercomponent/footercomponent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    SectionOneComponent
+    SectionOneComponent,
+    FootercomponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/footercomponent/footercomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footercomponent',
+  templateUrl: './footercomponent.component.html',
+  styleUrls: ['./footercomponent.component.sass']
+})
+export class FootercomponentComponent {
+
+}
