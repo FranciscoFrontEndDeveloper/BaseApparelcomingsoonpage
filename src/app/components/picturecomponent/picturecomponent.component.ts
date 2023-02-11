ec3f57e89ad6577fe17349939d883365aec33200
FRANCISCO <pachonetworking@gmail.com>
2023-02-11 17:25:19 -0500

    image mobile added

--- src/app/components/picturecomponent/picturecomponent.component.ts
+++ src/app/components/picturecomponent/picturecomponent.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-picturecomponent',
   templateUrl: './picturecomponent.component.html',
-  styleUrls: ['./picturecomponent.component.sass']
+  styleUrls: ['./picturecomponent.component.sass'],
 })
 export class PicturecomponentComponent {
+  sizePicture: string = ''
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.sizeMediaQuery();
+  }
 
+  sizeMediaQuery() {
+    if (window.innerWidth < 1440) {
+      this.sizePicture ='../../../assets/images/hero-mobile.jpg'
+      console.log('es movil');
+    } else {
+      this.sizePicture = '../../../assets/images/hero-desktop.jpg'
+      console.log(this.sizePicture)
+    }
+  }
 }
